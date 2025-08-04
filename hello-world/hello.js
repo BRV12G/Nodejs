e84615fff30ebc0786df08409837dbb2400ed54f
Bhairavi Gawas <bhairavigawas9@gmail.com>
2025-08-04 14:11:56 +0800

    feat: modules in nodejs

---
+++ hello-world/hello.js
@@ -0,0 +1,9 @@
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hello");
+
+const math = require ("./math");
+console.log("The sum is:", math.add(1,2));
+console.log("The subtraction value is:", math.sub(5,6));
+// console.log(add(1, 2));
